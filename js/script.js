c1298e817ef5b78c3215923b486657f6cf25dc4e
Ivan Glebov <64957045+ratatujef@users.noreply.github.com>
2020-07-26 12:23:49 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 window.addEventListener("DOMContentLoaded", function () {
   "use strict";
 
-  let burger = document.querySelector(".humburger")[0];
+  let burger = document.querySelector(".humburger"),
+    project = document.querySelector(".project");
 
-  burger.addEventListener("clic", () => {
+  burger.addEventListener("click", () => {
     if (burger.classList.contains("humburger_active")) {
       burger.classList.remove("humburger_active");
+      project.classList.remove("project_active");
     } else {
       burger.classList.add("humburger_active");
+      project.classList.add("project_active");
     }
   });
 });
