c789e87bec8afbb6dbec401183c0fbaa70d9b822
ratatujef <ratatujef@gmail.com>
2020-08-24 13:21:18 +0300

    js

--- js/script.js
+++ js/script.js
@@ -13,4 +13,17 @@ window.addEventListener("DOMContentLoaded", function () {
       project.classList.add("project_active");
     }
   });
+  //отправка почты
+
+  // let form = document.querySelector('form');
+
+  // form.addEventListener('submit', function(e){
+  //   e.preventDefault();
+  //   let formData = new FormData([form]);
+  //   let xhr = new XMLHttpRequest();
+  //   xhr.open('POST', "mailer/smart.php");
+  //   xhr.send(formData);
+  //   xhr.onload=()=>alert(xhr.response);
+  // })
+  
 });
