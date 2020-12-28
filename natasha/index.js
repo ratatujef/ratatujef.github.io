c1ebcc68f52e6ffd5a171a12d3334e3052dcf5ef
Ivan Glebov <64957045+ratatujef@users.noreply.github.com>
2020-12-28 19:20:36 +0300

    Update index.js

--- natasha/index.js
+++ natasha/index.js
@@ -2,4 +2,10 @@ const btn = document.querySelector('.button');
 const audio = document.querySelector('audio');
 btn.addEventListener('click', (e) => {
     audio.play();
-})
+});
+btn.addEventListener('touchstart', () => {
+    btn.style.transform = "scale(1.2)";
+});
+btn.addEventListener('touchend', () => {
+    btn.style.transform = "scale(1)";
+});
